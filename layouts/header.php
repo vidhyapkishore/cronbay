db4ae982645a02e902398507589098b9f1c7573b
Vidhya P K <119294731+vidhyapkishore@users.noreply.github.com>
2025-02-04 13:32:53 +0530

    Add files via upload

---
+++ layouts/header.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Hello, world!</title>
+  </head>
+  <body>
+
+    <header class=" d-flex flex-wrap px-5 align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+      <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+        CRONBAY
+      </a>
+
+      <!-- <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+        <li><a href="#" class="nav-link px-2 link-secondary">Home</a></li>
+        <li><a href="#" class="nav-link px-2 link-dark">Features</a></li>
+        <li><a href="#" class="nav-link px-2 link-dark">Pricing</a></li>
+        <li><a href="#" class="nav-link px-2 link-dark">FAQs</a></li>
+        <li><a href="#" class="nav-link px-2 link-dark">About</a></li>
+      </ul> -->
+
+      <div class="col-md-3 text-end">
+        <a class="link-dark text-decoration-none" href="login.php">Login</a> / <a class="link-dark text-decoration-none" href="register.php">Signup</a>
+        <a href="register.php" class="btn btn-success">Post a job</a>
+      </div>
+    </header>
